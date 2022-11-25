5c6b477a70cffd15fe210e8690e3234f7ae68b90
zhengxianh86 <zhengxianh86@gmail.com>
2022-11-25 14:59:23 +0800

    V1.0

---
+++ P6/P6/P6/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int cubeByValue(int n);
+
+int main()
+{
+	int number = 5;
+	printf("The original value of number is %d",number);
+
+	number = cubeByValue(number);
+
+	printf("\nThe new value of number is %d\n",number);
+}
+
+int cubeByValue(int n)
+{
+	return n * n*n;
+}
